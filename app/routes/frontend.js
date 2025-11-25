13e20cee68a91c1e89b835c0137e443c7ce352c0
EthanWonCho <realethanwoncho@gmail.com>
2025-11-25 21:40:01 +0900

    ts

--- app/routes/frontend.js
+++ app/routes/frontend.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import createError from "http-errors";
+import pool from '../utils/connectdb.js';
 
 const router = Router();
 
@@ -11,8 +12,8 @@ router.get('/applyroom', function(req, res) {
   res.render('applyroom');
 });
 
-router.get('/teacher', function(req, res) {
-  const [rst] = req.query('SELECT * FROM applications');
+router.get('/teacher', async function(req, res) {
+  const [rst] = await pool.query('SELECT * FROM applications');
   console.log(rst);
   res.render('teacher', { applies: rst });
 });
