2f862321284947e856f8df016aaaed2c89707310
EthanWonCho <realethanwoncho@gmail.com>
2025-11-25 20:59:05 +0900

    remove csrf

--- app/app.js
+++ app/app.js
@@ -121,7 +121,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(csurf());
+//app.use(csurf());
 
 app.use(passport.initialize());
 app.use(passport.session());
